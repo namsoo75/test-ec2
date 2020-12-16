ed6b503e3bb86d6a2789076feaa8befb6f7fc785
namsoo75 <mycallboy@naver.com>
2020-12-16 17:45:36 +0900

    no message

--- src/main/java/com/itad/qcheck/HomeController.java
+++ src/main/java/com/itad/qcheck/HomeController.java
@@ -26,72 +26,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	@Autowired
-	private JavaMailSender mailSender;
+
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		//logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		//Date date = new Date();
-		//DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
-		//String formattedDate = dateFormat.format(date);
+		String formattedDate = dateFormat.format(date);
 		
-		//model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("serverTime", formattedDate );
 		
-		return "index";
-	}
-	
-	@RequestMapping(value = "/question/mail", method = RequestMethod.POST)
-	public ResponseEntity<Void> homeQuestionMail(@RequestParam("name") String name, @RequestParam("email") String email, @RequestParam("questionText") String questionText) {
-		
-		ResponseEntity<Void> entity = null;
-		
-		logger.info(name);
-		logger.info(email);
-		logger.info(questionText);
-		
-    	try{
-            
-    		/*
-		MimeMessage message = mailSender.createMimeMessage();
-		MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
-		messageHelper.setTo("mycallboy@naver.com");
-		messageHelper.setText(text);
-		messageHelper.setFrom(email, "문의메일");
-		messageHelper.setSubject("title");	// 메일제목은 생략이 가능하다
-		*/
-    		
-    		SimpleMailMessage message = new SimpleMailMessage();
-
-    	    message.setFrom("tech_itad@itadtechnology.kr");
-
-    	    //message.setTo("mycallboy@naver.com");
-    	    message.setTo("tech_itad@itadtechnology.kr");
-
-    	    message.setSubject(name + "님의 문의 메일 입니다.");
-
-    	    message.setText("이름:[" + name + "] 이메일:[" + email + "]" + " 내용:[" + questionText + "]");
-
-    		
-    	    mailSender.send(message);
-		
-		
-    	    entity = new ResponseEntity<>( HttpStatus.OK);
-		
-		
-    	} catch (Exception e){
-    		e.printStackTrace();
-    		
-    		 entity = new ResponseEntity<>( HttpStatus.BAD_REQUEST );
-    	}
- 
-        return entity;
+		return "home";
 	}
 	
 }
